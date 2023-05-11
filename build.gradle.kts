7b0aab104a351e8dc3d40fc366b15d55c815522a
Alex Tetervak <tetervak@sheridancollege.ca>
2023-05-11 09:51:53 -0400

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.8.20"
+    application
+}
+
+group = "sheridan.tetervak"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    mainClass.set("MainKt")
+}
